ab6873befc9f8f2537d25e2afaf972af9a139304
Thomas Croizet <t.croizet77@gmail.com>
2022-11-28 23:01:35 +0100

    Fix project parameters

--- FilePollingWatcher/FilePollingWatcher.cs
+++ FilePollingWatcher/FilePollingWatcher.cs
@@ -1,5 +1,10 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace FilePollingWatcher
 {
